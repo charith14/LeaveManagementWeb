7e79682ab55eabbf344b5abaae2cecbe3bbf5cd5
charith <charithvishan100@gmail.com>
2021-03-07 13:37:07 +0530

    add controller

---
+++ LeaveManagementWeb/LeaveManagementWeb/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using LeaveManagementWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeaveManagementWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LeaveContext context = new LeaveContext();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public ActionResult LeaveApprove()
+        {
+            // have to display all leave applications for this Employee ID as the supervisor ID
+            List<LeaveEntry> leaveEntries = context.LeaveEntries.Where(x => x.Employee1.SupervisorCode == System.Web.HttpContext.Current.User.Identity.Name).ToList();
+            return View(leaveEntries);
+        }
+
+        public ActionResult LeaveApply()
+        {
+            ViewBag.empID = ViewBag.empId;
+            return View();
+        }
+
+       
+        [HttpPost]
+        public ActionResult LeaveApply(LeaveEntry leaveEntry)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+
+                    context.LeaveEntries.Add(leaveEntry);
+                    context.SaveChanges();
+
+                }
+                catch (Exception e)
+                {
+                    Console.Write("Error when entering the data" + e.Message);
+                    ModelState.AddModelError("", "Can't make your request. May be you have get all the leaves which are applicable 1");
+                }
+                return RedirectToAction("LeaveApply");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Can't make your request. May be you have get all the leaves which are applicable 2");
+                return View();
+            }
+
+        }
+   
+
+        public bool isValidateLeave(string empCode, string leaveCode)
+        {
+
+            //check all the fields are filled and ok with the business logic
+            bool validate = false;
+            LeaveAllocation leaveAllocation = context.LeaveAlloations.Where(x => x.EmployeeID == empCode && x.LeaveTypeCode.ToString() == leaveCode).SingleOrDefault();
+
+            if (leaveAllocation.UtilizedAmount < leaveAllocation.EntitledAmount)
+            {
+                //all the leaves has been get
+                validate = true;
+
+            }
+
+
+            return validate;
+        }
+
+        //  [Authorize]
+        public ActionResult approve(string empId, string leaveTypeId, DateTime reqDate)
+        {
+            LeaveEntry lEntry = context.LeaveEntries.Where(x => x.EmployeeCode == empId && x.LeaveTypeId == leaveTypeId && x.RequestedDate == reqDate).SingleOrDefault();
+            return View(lEntry);
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult approve(string empId, string leaveTypeId, DateTime reqDate, LeaveEntry leaveEntry)
+        {
+            LeaveEntry lEntry = context.LeaveEntries.Where(x => x.EmployeeCode == empId && x.LeaveTypeId == leaveTypeId && x.RequestedDate == reqDate).SingleOrDefault();
+
+            if (ModelState.IsValid)
+            {
+
+                //update
+                lEntry.EmployeeCode = leaveEntry.EmployeeCode;
+                lEntry.LeaveTypeCode = leaveEntry.LeaveTypeCode;
+                lEntry.RequestedDate = leaveEntry.RequestedDate;
+                lEntry.CheckByName = leaveEntry.CheckByName;
+                lEntry.CheckDate = DateTime.Now;
+                lEntry.Status = "Approved";
+                lEntry.CommentsEmployee = leaveEntry.CommentsEmployee;
+                lEntry.CommentsApprover = leaveEntry.CommentsApprover;
+                lEntry.AutoNumber = leaveEntry.AutoNumber;
+                context.SaveChanges();
+
+                return RedirectToAction("LeaveApprove");
+
+            }
+            else
+            {
+                return View(lEntry);
+            }
+
+
+        }
+    }
+}
--- LeaveManagementWeb/LeaveManagementWeb/Models/LeaveEntry.cs
+++ LeaveManagementWeb/LeaveManagementWeb/Models/LeaveEntry.cs
@@ -9,47 +9,47 @@ namespace LeaveManagementWeb.Models
 {
     public class LeaveEntry
     {
+
         [Key, Column(Order = 0)]
         [Editable(false)]
         [Required(ErrorMessage = "Employee Code is required.")]
         public string EmployeeCode { get; set; }
 
 
+        public string CheckByName { get; set; } 
+
         [Key, Column(Order = 1)]
         public string LeaveTypeId { get; set; }
-
 
         [Key, Column(Order = 2)]
         [DisplayFormat(DataFormatString = "{yyyy-MM-dd}")]
         [DataType(DataType.Date)]
         public DateTime RequestedDate { get; set; }
 
-
         [DataType(DataType.Date)]
-        public DateTime CheckDate { get; set; } // Leave approved or rejected date
+        public DateTime CheckDate { get; set; } 
 
-        public string CheckByName { get; set; } // approved or rejected by
-
-        public string Status { get; set; } // Requested, Approved, Cancelled
+        public string Status { get; set; } 
 
 
-        public string CommentsEmployee { get; set; } // Comments By Employee
+        public string CommentsEmployee { get; set; } 
 
 
-        public string CommentsApprover { get; set; } // Comments by Approver
+        public string CommentsApprover { get; set; } 
 
 
         public int AutoNumber { get; set; }
 
         [ForeignKey("EmployeeCode"), Column(Order = 0)]
         public virtual Employee Employee1 { get; set; }
-        // public ICollection<Employee> Employee1 { get; set; }
+       
 
         [ForeignKey("CheckByName"), Column(Order = 1)]
         public virtual Employee Employee2 { get; set; }
-        //public ICollection<Employee> Employee2 { get; set; }
+        
 
         [ForeignKey("LeaveTypeId"), Column(Order = 2)]
         public virtual LeaveType LeaveTypeCode { get; set; }
+
     }
 }
